7d458bbcb3d3389c7574cb4191e28c02b0943bf0
Eriton Sousa <eritondev22@gmail.com>
2022-12-20 12:30:01 -0300

    arrays + objects

---
+++ arrays/arrays.js
@@ -0,0 +1,20 @@
+const animals = [
+    'Lion', //indice = 0
+    'Monkey', // indice = 1
+    {
+        name: 'Cat', // essa chave é do indice = 2
+        age: 3
+    }
+]
+
+console.log(animals[0]) //Acessando o indice do array
+
+console.log(animals[2].name) // Acessando o indice do object do array
+
+
+
+/* 
+    ARRAYS NÃO precisa de como se fosse uma variável e atribuir um valor (name: 'Eriton).
+        Mas, pode colocar Strings, number, OBJECTS...
+
+*/
---
+++ object/object.js
@@ -0,0 +1,11 @@
+const person = {
+    name: 'Eriton',
+    age: 22,
+    weight: 69.5,
+    isAdmin: true
+}
+
+console.log(person.name)
+console.log(`${person.name} tem ${person.age} anos`),
+
+/* OBJECTS precisa de como se fosse uma variável e atribuir um valor (name: 'Eriton) */

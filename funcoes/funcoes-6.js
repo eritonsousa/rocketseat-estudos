8563fc0543f17b3a16904613d4455976f191a5c4
Eriton Sousa <eritondev22@gmail.com>
2022-12-22 02:21:02 -0300

    .......

--- funcoes/funcoes-6.js
+++ funcoes/funcoes-6.js
@@ -5,3 +5,11 @@
     *criar um novo objeto
     *this keyword
 */
+
+function Person(name){
+    this.name = name
+}
+const eriton = new Person('Primeiro Nome')
+const sousa = new Person('Sobrenome')
+console.log(eriton)
+console.log(sousa)
